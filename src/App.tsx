91704befbc8ffa53ea9f82fbc016654f3cd543e8
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-02-10 13:06:28 +0100

    Refactor show-hide function. Added id to li element to avoid multiple li with same classname from different parent

--- src/App.tsx
+++ src/App.tsx
@@ -132,7 +132,7 @@ const App = () => {
           <ul>
           {nodes?.map((node) =>
             <li key={node.name}>
-              {node.name} {node.nodesChild.length > 0 && <Button onClick={(e) => handleShowHide(node.name)} >Zwiń / Rozwiń</Button>}
+              {node.name} {node.nodesChild.length > 0 && <Button onClick={(e) => handleShowHide(node.name+node.id.toString())} >Zwiń / Rozwiń</Button>}
               {node.nodesChild.length > 0 && <Button onClick={(e) => handleSortNode(node.name)} >Sortuj</Button>}
               {node.nodesChild.length > 0 && <NodeTree id={node.id} parentId={node.parentId} name={node.name} nodesChild={node.nodesChild} />}
             </li>
--- src/components/NodeTree.tsx
+++ src/components/NodeTree.tsx
@@ -8,8 +8,8 @@ export const NodeTree = (nodes:INodeTree) => {
     return (
           <ul id={nodes.name}>
           {nodes.nodesChild.map((node) => 
-            <li className={nodes.name} key={node.name}>
-              {node.name} {node.nodesChild.length > 0 && <Button onClick={(e) => handleShowHide(node.name)} >Zwiń / Rozwiń</Button>}
+            <li className={nodes.name+nodes.id.toString()} key={node.name}>
+              {node.name} {node.nodesChild.length > 0 && <Button onClick={(e) => handleShowHide(node.name+node.id.toString())} >Zwiń / Rozwiń</Button>}
               {node.nodesChild.length > 0 && <Button onClick={(e) => handleSortNode(node.name)} >Sortuj</Button>}
               {node.nodesChild.length > 0 && <NodeTree id={node.id} parentId={node.parentId} name={node.name} nodesChild={node.nodesChild} />}
             </li>
