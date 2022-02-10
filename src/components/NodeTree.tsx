e03e5ce3ad036da6d728d9a52cfc6e5da472f486
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-02-10 12:55:28 +0100

    Refactor button for show and hide

--- src/components/NodeTree.tsx
+++ src/components/NodeTree.tsx
@@ -9,7 +9,7 @@ export const NodeTree = (nodes:INodeTree) => {
           <ul id={nodes.name}>
           {nodes.nodesChild.map((node) => 
             <li className={nodes.name} key={node.name}>
-              {node.name} {node.nodesChild.length > 0 && <Button onClick={(e) => handleShowHide(node.name)} >+/-</Button>}
+              {node.name} {node.nodesChild.length > 0 && <Button onClick={(e) => handleShowHide(node.name)} >Zwiń / Rozwiń</Button>}
               {node.nodesChild.length > 0 && <Button onClick={(e) => handleSortNode(node.name)} >Sortuj</Button>}
               {node.nodesChild.length > 0 && <NodeTree id={node.id} parentId={node.parentId} name={node.name} nodesChild={node.nodesChild} />}
             </li>
